343eea398c597493f7c7d67656524a0ff97684ec
TachyonFTL <40037251+TachyonFTL@users.noreply.github.com>
2018-12-15 12:24:49 -0600

    Delete raw_to_text.py

--- raw/raw_to_text.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-raw = pd.read_csv('LIWC2015 Results (1000_tweets_per_user.csv).csv', encoding='utf-8')
-print(raw)
-raw.to_csv('LIWC2015 Results 100', sep='\t', encoding='utf-8', index=False)
