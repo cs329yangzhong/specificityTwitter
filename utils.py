691b8f50686d02b14018247d0dc74ee4bbed2e29
YangCSZhong <31517298+cs329yangzhong@users.noreply.github.com>
2021-10-19 14:26:37 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,10 +6,10 @@ import numpy as np
 
 RT = "./"
 
-BROWNCLUSFILE_100 = RT + "resources/MetaOptimize/BrownWC/brown-rcv1.clean.tokenized-CoNLL03.txt-c100-freq1.txt"
-BROWNCLUSFILE = RT + "resources/MetaOptimize/BrownWC/bronwn-rcv1-1000-freq1.txt"
+BROWNCLUSFILE_100 = RT + "model/resources/MetaOptimize/BrownWC/brown-rcv1.clean.tokenized-CoNLL03.txt-c100-freq1.txt"
+BROWNCLUSFILE = RT + "model/resources/MetaOptimize/BrownWC/bronwn-rcv1-1000-freq1.txt"
 
-STOPWORDFILE = RT + "resources/nltkstopwords.txt"
+STOPWORDFILE = RT + "model/resources/nltkstopwords.txt"
 
 def readMetaOptimizeBrownCluster_100():
     print ("loading brown clusters...")
